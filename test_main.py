051e85d3a179f1ac41536d8d20a8c4df05c8e430
Milan M. <148401452+MT25MB@users.noreply.github.com>
2025-02-24 23:20:10 -0500

    Update test_main.py
    thats crazy

--- test_main.py
+++ test_main.py
@@ -1,6 +1,8 @@
 import unittest
 from main import password_strength_checker
 
+# IGNORE ALL OF THIS
+
 class TestPasswordStrengthChecker(unittest.TestCase):
     def test_weak_password(self):
         self.assertEqual(password_strength_checker("123"), "Very Weak")
